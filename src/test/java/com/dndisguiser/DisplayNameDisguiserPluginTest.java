ec22697662259c42e0b713dbedc58e0fabfd7c8c
YaIPartake <162069294+YaIPartake@users.noreply.github.com>
2024-04-06 13:59:33 -0500

    Updated package name

--- src/test/java/com/dndisguiser/DisplayNameDisguiserPluginTest.java
+++ src/test/java/com/dndisguiser/DisplayNameDisguiserPluginTest.java
@@ -1,8 +1,7 @@
-package net.runelite.client.plugins.displaynamedisguiser.src.test.java.com.dndisguiser;
+package com.displaynamedisguiser;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
-import net.runelite.client.plugins.displaynamedisguiser.src.main.java.com.dndisguiser.DisplayNameDisguiserPlugin;
 
 public class DisplayNameDisguiserPluginTest
 {
